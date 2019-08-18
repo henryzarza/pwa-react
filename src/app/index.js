5644cfcdd8f75d82a65c493e5fcc43d8d3bbb122
Henry Zarza <henryacz7@gmail.com>
2019-08-18 13:53:04 -0500

    Starter files and folder structure

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Characters from './screens/Characters';
+
+function App() {
+  return (
+    <div>
+      This is the App
+      <Characters />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/screens/Characters/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Characters() {
+  return (
+    <div>
+      Characters screen
+    </div>
+  );
+}
+
+export default Characters;
