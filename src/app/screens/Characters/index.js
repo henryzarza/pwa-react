8935201aab608ab988ad5a6de43187ce8c36593d
Henry Zarza <henryacz7@gmail.com>
2019-08-21 09:11:18 -0500

    Get data from indexedDB

--- src/app/screens/Characters/index.js
+++ src/app/screens/Characters/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { axiosInstance } from '../../../config/api';
 import Card from '../../components/Card';
 import Loader from '../../components/Loader';
-import { saveData } from '../../../services/indexedDB';
+import { createDataBase, getHeroes } from '../../../services/indexedDB';
 import { showNotification } from '../../../services/notification';
 
 import './style.css';
@@ -13,18 +13,33 @@ class Characters extends Component {
 
   componentDidMount() {
     if (navigator.onLine) {
-      axiosInstance.get('/characters')
-        .then(response => {
-          saveData(response.data);
-          this.setState({ data: response.data, loading: false });
-          showNotification("This is site work offline. It's PWA :)");
-        })
-        .catch(error => console.log('Error', error))
+      this.getData();
     } else {
-      // GET data from indexedDB
+      this.getOfflineData();
       showNotification("Connection not available but keep calm we're PWA");
     }
   }
+
+  getOfflineData = async () => {
+    let data = await getHeroes();
+    data = data.map(hero => {
+      const photo = URL.createObjectURL(hero.photo);
+      return { ...hero, photo };
+    });
+    this.setState({ data, loading: false });
+  };
+
+  getData = async () => {
+    const response = await axiosInstance.get('/characters');
+    try {
+      createDataBase(response.data);
+      this.setState({ data: response.data, loading: false });
+      showNotification("This is site work offline. It's PWA :)");
+    } catch (error) {
+      this.setState({ loading: false });
+      showNotification(`Oops error: ${error}`);
+    }
+  };
 
   render() {
     const { data, loading } = this.state;
--- src/services/indexedDB.js
+++ src/services/indexedDB.js
@@ -1,7 +1,7 @@
 const DATABASE_NAME = 'DCHeroes';
 const OBJECT_STORE_NAME = 'heroes';
 
-export const saveData = heroes => {
+export const createDataBase = heroes => {
   if (!window.indexedDB) {
     console.warn("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
     return;
@@ -9,44 +9,78 @@ export const saveData = heroes => {
   // Let us open our database
   const request = window.indexedDB.open(DATABASE_NAME, 2);
 
-  request.onerror = event => {
-    console.error("IndexedDB error: ", event.target.errorCode);
+  request.onerror = () => {
+    console.log('Error: ', request.error.message);
   };
 
   request.onupgradeneeded = event => {
     const db = event.target.result;
-    // Create an objectStore to hold information about our heroes. We're
-    // going to use "id" as our key path because it's guaranteed to be unique
-    // if (!db.objectStoreNames.contains(OBJECT_STORE_NAME)) {
+    // Create an objectStore to hold information about our heroes. We're going to use "id" as our key path because it's guaranteed to be unique
     const objectStore = db.createObjectStore(OBJECT_STORE_NAME, { keyPath: "id" });
 
     // Creation of indexes (data of the heroes)
     objectStore.createIndex("id", "id", { unique: true });
     objectStore.createIndex("name", "name", { unique: true });
-    objectStore.createIndex("photo", "photo", { unique: false }); // replace this by Blob
+    objectStore.createIndex("photo", "photo", { unique: false });
     objectStore.createIndex("realName", "realName", { unique: false });
     objectStore.createIndex("description", "description", { unique: false });
     objectStore.createIndex("height", "height", { unique: false });
     objectStore.createIndex("weight", "weight", { unique: false });
-    // }
 
-    // console.log(db);
-
-    // Use transaction oncomplete to make sure the objectStore creation is 
-    // finished before adding data into it.
+    // Use transaction oncomplete to make sure the objectStore creation is finished before adding data into it.
     objectStore.transaction.oncomplete = () => {
-      const heroObjectStore = db.transaction([OBJECT_STORE_NAME], "readwrite").objectStore(OBJECT_STORE_NAME);
-      heroes.forEach(hero => heroObjectStore.add(hero));
+      saveData(heroes);
     };
+  };
+
+  request.onsuccess = () => {
+    saveData(heroes);
   };
 };
 
+const saveData = heroes => {
+  heroes.forEach(async hero => {
+    const photo = await convertImageToBlob(hero.photo);
+    saveHero({ ...hero, photo });
+  });
+};
+
+const convertImageToBlob = async url => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.send();
+  });
+}
+
+const saveHero = hero => {
+  const IdDB = window.indexedDB.open(DATABASE_NAME, 2);
+  IdDB.onsuccess = async event => {
+    const db = event.target.result;
+    const objectStore = db.transaction([OBJECT_STORE_NAME], "readwrite").objectStore(OBJECT_STORE_NAME);;
+    objectStore.add(hero).onsuccess = () => db.close();
+  }
+}
+
 export const getHeroes = () => {
   const IdDB = window.indexedDB.open(DATABASE_NAME, 2);
-
-  return IdDB.onsuccess = event => {
-    const db = event.target.result;
-    const objectStore = db.transaction([OBJECT_STORE_NAME]).objectStore(OBJECT_STORE_NAME);
-    return objectStore.getAll();
-  }
-};
+  return new Promise(resolve => {
+    IdDB.onsuccess = async event => {
+      const db = event.target.result;
+      const objectStore = db.transaction([OBJECT_STORE_NAME]).objectStore(OBJECT_STORE_NAME);
+      objectStore.getAll().onsuccess = result => {
+        db.close();
+        resolve(result.target.result);
+      }
+    }
+  });
+}
